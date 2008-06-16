fb750cf4401b2a4f267cf390ad48440e382c1587
Dirk Breuer <dbreuer@p249206.public.FH-Koeln.DE>
2008-06-16 13:33:20 +0200

    CHANGED - JMF laesst sich nicht sinnvoll integrieren. das Medien Objekt wird entsprechend anders verwendet. refs #10

--- code/cosima-framework/src/main/java/de/fhkoeln/cosima/media/MediaIO.java
+++ code/cosima-framework/src/main/java/de/fhkoeln/cosima/media/MediaIO.java
@@ -4,4 +4,9 @@
 package de.fhkoeln.cosima.media;
 
 public interface MediaIO {
+  public MediaLocator getMediaLocator();
+  
+  public void setMediaLocator(MediaLocator mediaLocator);
+  
+  public MediaControls getMediaControls();
 }
---
+++ code/cosima-framework/src/main/java/de/fhkoeln/cosima/media/MediaLocator.java
@@ -0,0 +1,26 @@
+/*
+ * MediaLocator.java
+ *
+ * Version 1.0  Jun 12, 2008
+ *
+ * Copyright notice
+ *
+ * Brief description
+ *
+ * (c) 2008 by dbreuer
+ */
+package de.fhkoeln.cosima.media;
+
+
+/**
+ * Documentation comment without implementation details. 
+ * Use implementation comments to describe details of the implementation.
+ * Comment lines should not be longer than 70 characters.
+ *
+ * @author dbreuer
+ * @version 1.0  Jun 12, 2008
+ *
+ */
+public interface MediaLocator {
+
+}
