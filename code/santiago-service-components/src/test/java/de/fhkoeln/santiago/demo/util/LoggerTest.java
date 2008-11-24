bcc6658d4aecb83c135c7b7e3b8bca1f80de63aa
Dirk Breuer <dirk.breuer@gmail.com>
2008-11-25 00:45:13 +0100

    ADDED - Logging fuer die services. refs #31

---
+++ code/santiago-service-components/src/test/java/de/fhkoeln/santiago/demo/util/LoggerTest.java
@@ -0,0 +1,34 @@
+/*
+ * LoggerTest.java
+ *
+ * Version 1.0  Nov 24, 2008
+ *
+ * Copyright notice
+ *
+ * Brief description
+ *
+ * (c) 2008 by dbreuer
+ */
+package de.fhkoeln.santiago.demo.util;
+
+import org.junit.Test;
+
+
+
+/**
+ * Documentation comment without implementation details. 
+ * Use implementation comments to describe details of the implementation.
+ * Comment lines should not be longer than 70 characters.
+ *
+ * @author dbreuer
+ * @version 1.0  Nov 24, 2008
+ *
+ */
+public class LoggerTest {
+  
+  @Test
+  public void testShouldLogSomething() throws Exception {
+    Logger.debug("This is a Debug Message");
+  }
+  
+}
